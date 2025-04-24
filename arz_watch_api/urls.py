5773aa3b85a67458dbe30621787a7dc594e3fcdf
pouria-drd <pouriadrdgmail.com>
2025-04-24 14:38:50 +0330

    Only add base_url once at the root level

--- arz_watch_api/urls.py
+++ arz_watch_api/urls.py
@@ -11,17 +11,19 @@ load_dotenv()
 
 router = routers.DefaultRouter()
 
-
 base_url: str = os.getenv("BASE_URL", "")
 
 urlpatterns = [
     path(base_url, include(router.urls)),
-    # main admin
-    path(base_url + os.getenv("ADMIN_URL", "admin/"), admin.site.urls),
-    # api urls
-    path(base_url + "scrapers/", include("scrapers.urls")),
-    path(base_url + "telegram/", include("telegram.urls")),
+    path(
+        os.getenv("ADMIN_URL", "admin/"), admin.site.urls
+    ),  # Admin URL without base_url
+    path("scrapers/", include("scrapers.urls")),
+    path("telegram/", include("telegram.urls")),
 ]
+
+# Only add base_url once at the root level
+urlpatterns = [path(base_url, include(urlpatterns))]
 
 if settings.DEBUG:
     urlpatterns += (path("__debug__/", include("debug_toolbar.urls")),)
